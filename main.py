a1a32dd91f45b73852b1fc9827c5e3d01250c306
mattblake <mjb409@uclive.ac.nz>
2020-08-06 13:21:49 +1200

    made functions

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from scipy.signal import freqz, lfilter, firwin, remez, convolve
 
-filename = 'enel420_grp_18.txt'
+def importData(filename):
+    """Import data from a text file"""
 
-data = open(filename, 'r')
-XDDD = data.read()
-XD = XDDD.split()
+    # Extract data from file
+    data_file = open(filename, 'r') # Create the file object where data is stored
+    data_string = data_file.read() # Read the data from the file object
+    data_list = data_string.split() # Create a list of each sample from the singular data string
 
-for sample in XD:
-    print(float(sample))
+    # Convert data from strings to floats
+    data = [] # Create array for results to be stored in
+    for data_string in data_list:
+        sample = float(data_string)
+        data.append(sample)
 
+    return data
+
+
+def main():
+    """Main function of ENEL420 Assignment 1"""
+
+    filename = 'enel420_grp_18.txt' # Location in project where data is stored
+
+    samples = importData(filename) # Import data from file
+
+
+main()
