9d98b4409135187fbc37c788d4bf04036f34b4d6
mattblake <mjb409@uclive.ac.nz>
2020-08-06 13:11:00 +1200

    importeddatas

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from scipy.signal import freqz, lfilter, firwin, remez, convolve
 
-msg = "hello"
-print(msg)
+filename = 'enel420_grp_18.txt'
+
+data = open(filename, 'r')
+XDDD = data.read()
+XD = XDDD.split()
+
+for sample in XD:
+    print(float(sample))
+
