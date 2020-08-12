71d117dda351b6346e15103288e1380d64700020
mattblake <mjb409@uclive.ac.nz>
2020-08-12 17:50:17 +1200

    tidy

--- main.py
+++ main.py
@@ -127,35 +127,6 @@ def combineFilters(numerator_1, denominator_1, numerator_2, denominator_2):
 
 
 
-def calculateVariance(data):
-    """Calculates and returns the variance of a signal"""
-
-    # Calculate the variance of the signal X using: variance = E[X^2] - E[X]^2
-    expected_data_power = sum((np.square(data)))/len(data)  # Calculate E[X^2]
-    power_of_expected_data = np.square(sum(data)/len(data))  # Calculate E[X]^2
-    variance_data = expected_data_power - power_of_expected_data  # Calculate the variance of the data
-
-    return variance_data
-
-
-
-def calculateNoiseVariance(data, filtered_data):
-    """"Calculate the variance of the noise by comparing the filtered and unfiltered data. The variance of the noise
-    is approximated as the variance of the signal removed by the filter"""
-
-    # Turn data arrays into numpy arrays so that mathematical operations can be performed
-    np_data = np.array(data)
-    np_filtered_data = np.array(filtered_data)
-
-    # Calculate the variance of the removed noise by finding the variances of the filtered and unfiltered data
-    data_variance = calculateVariance(np_data) # Calculate the variance of the unfiltered data
-    filtered_data_variance = calculateVariance(np_filtered_data) # Calculat the variance of the filtered data
-    noise_data_variance = data_variance - filtered_data_variance # Calculate the variance of the removed noise
-
-    return noise_data_variance
-
-
-
 def createWindowFilter(notches, sample_rate, notch_width):
     """Compute and return the bandstop  window filter array for the specified notches. Adjusting the window type and band width changes attenuation."""
 
@@ -238,6 +209,35 @@ def saveFigures(figures, figures_location, figure_names):
     for i in range(len(figures)):
         plt.figure(figures[i].number) # Set the figure as the current figure
         plt.savefig(output_path + '/' + figure_names[i]) # Save the current figure
+
+
+
+def calculateVariance(data):
+    """Calculates and returns the variance of a signal"""
+
+    # Calculate the variance of the signal X using: variance = E[X^2] - E[X]^2
+    expected_data_power = sum((np.square(data)))/len(data)  # Calculate E[X^2]
+    power_of_expected_data = np.square(sum(data)/len(data))  # Calculate E[X]^2
+    variance_data = expected_data_power - power_of_expected_data  # Calculate the variance of the data
+
+    return variance_data
+
+
+
+def calculateNoiseVariance(data, filtered_data):
+    """"Calculate the variance of the noise by comparing the filtered and unfiltered data. The variance of the noise
+    is approximated as the variance of the signal removed by the filter"""
+
+    # Turn data arrays into numpy arrays so that mathematical operations can be performed
+    np_data = np.array(data)
+    np_filtered_data = np.array(filtered_data)
+
+    # Calculate the variance of the removed noise by finding the variances of the filtered and unfiltered data
+    data_variance = calculateVariance(np_data) # Calculate the variance of the unfiltered data
+    filtered_data_variance = calculateVariance(np_filtered_data) # Calculat the variance of the filtered data
+    noise_data_variance = data_variance - filtered_data_variance # Calculate the variance of the removed noise
+
+    return noise_data_variance
 
 
 
